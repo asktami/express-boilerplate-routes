9d71d946d7ada98c8547c8d4d3b81234ee6be98d
Tami Williams <tami@asktami.com>
2019-10-14 18:29:41 -0400

    separated out validation and error handler

--- src/app.js
+++ src/app.js
@@ -1,55 +1,36 @@
 require('dotenv').config();
 const express = require('express');
-const logger = require('./logger');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-
 const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
 
 // ROUTERS
 const cardRouter = require('./card/card-router');
 
 const app = express();
 
-// const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
-// app.use(morgan(morganOption));
+app.use(
+	morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+		skip: () => NODE_ENV === 'test'
+	})
+);
 
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 app.use(helmet.hidePoweredBy());
 
-// get to / without authorization
+app.use(validateBearerToken);
+
+// USE ROUTERS
+app.use(cardRouter);
+
 app.get('/', (req, res) => {
 	res.send('Hello, world!');
 });
 
-//  validate that an Authorization header with an API token is present
-app.use(function validateBearerToken(req, res, next) {
-	const apiToken = process.env.API_TOKEN;
-	const authToken = req.get('Authorization');
-
-	if (!authToken || authToken.split(' ')[1] !== apiToken) {
-		// If the authorization fails log the error
-		logger.error(`Unauthorized request to path: ${req.path}`);
-		return res.status(401).json({ error: 'Unauthorized request' });
-	}
-	// move to the next middleware
-	next();
-});
-
-// USE ROUTER
-app.use(cardRouter);
-
-app.use(function errorHandler(error, req, res, next) {
-	let response;
-	if (NODE_ENV === 'production') {
-		response = { error: { message: 'server error' } };
-	} else {
-		console.error(error);
-		response = { message: error.message, error };
-	}
-	res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
